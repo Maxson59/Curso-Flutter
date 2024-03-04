1cfb51cbb8ae66dacb2c588bf2ff4f7c06299aa0
Oscar Marin <oscarmariomarin@gmail.com>
2024-03-03 22:28:24 -0600

    FEAT(counter/lib/): Added the modules showed in the course to develop the counter app.

---
+++ counter/lib/presentantion/screens/counter/counter_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text(('Counter Screen')),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  [
+            Text('$clickCounter', style: const TextStyle(
+              fontSize: 160,
+              fontWeight: FontWeight.w100
+            ),),
+            Text('Click${clickCounter == 1 ? '':'s'}', style: const TextStyle(
+              fontSize: 25))
+          ],
+      )),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+        FloatingActionButton(
+        child: const Icon(Icons.plus_one),
+        onPressed: (){
+          setState(() {
+            clickCounter++;
+          });
+
+          // clickCounter++;
+          // setState(() {});
+
+        },
+
+      ),
+      FloatingActionButton(
+        child: const Icon(Icons.refresh_outlined),
+        onPressed: (){
+          setState(() {
+            clickCounter = 0;
+          });
+
+          // clickCounter++;
+          // setState(() {});
+
+        },
+
+      )
+      ],)
+    );
+
+  }
+}
